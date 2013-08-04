35171e6c509029777fbcba5d77bea3afc12620dc
Micah Chalmer <micah@micahchalmer.net>
2013-08-04 02:09:32 -0400

    More WIP stuff

--- src/fuse/lib.rs
+++ src/fuse/lib.rs
@@ -9,7 +9,16 @@ url = "https://github.com/MicahChalmer/rust-fuse")];
 
 #[link_args = "-lfuse"] 
 
-use std::libc::{c_int, c_char, c_void, size_t};
+use std::libc::{
+    c_char,
+    c_int,
+    c_uint,
+    c_ulong,
+    c_void,
+    off_t,
+    size_t,
+    stat
+};
 
 // A cfuncptr is used here to stand in for a C function pointer.
 // For this struct, see fuse.h
@@ -66,10 +75,42 @@ struct c_fuse_operations {
     flock: cfuncptr
 }
 
+pub struct fuse_file_info {
+    flags: c_int,
+    fh_old: c_ulong, // Old file handle, don't use
+    writepage: c_int,
+    direct_io: c_uint,
+    keep_cache: c_uint,
+    flush: c_uint,
+    nonseekable: c_uint,
+    flock_release: c_uint,
+    padding: c_uint, // Padding.  Do not use
+    fh: u64,
+    lock_owner: u64
+}
+
 extern {
     fn fuse_main_real(argc:c_int, argv:**c_char, 
                       op:*c_fuse_operations, op_size: size_t,
                       user_data: *c_void);
 }
 
+// Used for return values from FS operations
+type errno = int;
 
+pub struct dir_entry {
+    name: ~str,
+    stat: Option<stat>,
+    off: off_t
+}
+
+pub trait FuseOperations {
+    fn getattr(path:&str) -> Either<stat, errno>;
+    fn readdir(path:&str, info: &fuse_file_info) -> (errno, ~[~dir_entry]);
+    fn open(path:&str, info: &mut fuse_file_info) -> errno;  // TODO: don't allow mutation of the whole fuse_file_info
+    fn read(path:&str, buf:&mut [u8], size: size_t, offset: off_t, info: &fuse_file_info) -> (errno, size_t);
+}
+
+pub fn fuse_main<T: FuseOperations>(args: ~[~str], ops: ~T) {
+
+}
