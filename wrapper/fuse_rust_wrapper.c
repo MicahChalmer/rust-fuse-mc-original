81e6f0f13299b0ab8e63cf1b35796ffff8ad3ca9
Micah Chalmer <micah@micahchalmer.net>
2013-08-04 15:37:10 -0400

    C wrapper WIP

---
+++ wrapper/fuse_rust_wrapper.c
@@ -0,0 +1,7 @@
+#include <fuse.h>
+
+int call_filler_function(fuse_fill_dir_t filler, void *buf, const char *name,
+                         const struct stat *stbuf, off_t off)
+{
+  return filler(buf, name, stbuf, off);
+}
