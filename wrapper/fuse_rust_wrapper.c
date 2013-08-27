db0b31b7ad2156c76b9d07887ecc3020ac438527
Micah Chalmer <micah@micahchalmer.net>
2013-08-26 23:59:50 -0400

    Remove C wrapper; no longer needed

--- wrapper/fuse_rust_wrapper.c
+++
@@ -1,7 +0,0 @@
-#include <fuse.h>
-#include <stdio.h>
-
-int call_filler_function(fuse_fill_dir_t filler, void *buf, const char *name,
-                         const struct stat *stbuf, off_t off) {
-  return filler(buf, name, stbuf, off);
-}
